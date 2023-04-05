a8949a0fee1452208cf2540bdbca1f9b1c7a63d0
Johanna Toivanen <johanna.toivanen@metropolia.fi>
2023-04-05 10:31:27 +0300

    removed unused code from burger class

--- src/builder/Burger.java
+++ src/builder/Burger.java
@@ -1,20 +1,9 @@
 package builder;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Burger {
     private String components;
 
     public Burger(String components) {
-        this.components = components;
-    }
-
-    public String getComponents() {
-        return components;
-    }
-
-    public void setComponents(String components) {
         this.components = components;
     }
 
